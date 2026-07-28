e9f024ef8e2faab735240691bf83c58ca1a974b6
Bishes Maharjan <maharjanbichheach0909@gmail.com>
2026-07-28 13:51:50 +0545

    changed jorunal to blog

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -71,7 +71,7 @@ export default async function BlogPostPage({ params }: { params: Promise<{ slug:
           <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M19 12H5M5 12L12 19M5 12L12 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
-          Back to Journal
+          Back to Blog
         </Link>
 
         <header className="blog-post-header">
